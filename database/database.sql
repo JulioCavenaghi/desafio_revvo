4673c2aeeda268e00de18471364d43038dbc95bb
JulioCavenaghi <70040646+JulioCavenaghi@users.noreply.github.com>
2025-07-16 20:42:36 -0300

    Fix: Adiciona o campo url a tabela courses

--- database/database.sql
+++ database/database.sql
@@ -4,8 +4,9 @@ USE desafio_revvo;
 
 CREATE TABLE courses (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  image VARCHAR(255) NOT NULL,
-  description TEXT,
+  title VARCHAR(80) NOT NULL,
+  image TEXT NOT NULL,
+  description varchar(500),
+  url varchar(2083) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
