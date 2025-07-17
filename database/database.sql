96f8f75915fc9c06ee433b62e4a03592bfa135d5
JulioCavenaghi <70040646+JulioCavenaghi@users.noreply.github.com>
2025-07-16 22:14:42 -0300

    Fix: Ajusta o tipo de campo de image para poder salvar um base64

--- database/database.sql
+++ database/database.sql
@@ -5,7 +5,7 @@ USE desafio_revvo;
 CREATE TABLE courses (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(80) NOT NULL,
-  image TEXT NOT NULL,
+  image LONGTEXT NOT NULL,
   description varchar(500),
   url varchar(2083) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
